82a95318b399f51cc7f32a4bdd5e7ca1a47962c0
Simon Julier <s.julier@ucl.ac.uk>
2021-06-03 15:31:22 +0100

    Removed unneeded cmake definition.

--- Scripts/Build_Source.sh
+++ Scripts/Build_Source.sh
@@ -26,6 +26,6 @@ echo Build type ${build_type} using ${num_jobs} parallel jobs
 root_dir=`pwd`
 mkdir -p Build/${build_type}/Source
 pushd Build/${build_type}/Source > /dev/null
-cmake ${root_dir}/Source -DCMAKE_BUILD_TYPE=${build_type} -DCMAKE_INSTALL_PREFIX=${BUILD_INSTALL_PREFIX} -DDEFAULT_VOC_FILE=${BUILD_INSTALL_PREFIX}/var/lib/orbslam2/ORBvoc.txt
+cmake ${root_dir}/Source -DCMAKE_BUILD_TYPE=${build_type} -DCMAKE_INSTALL_PREFIX=${BUILD_INSTALL_PREFIX}
 make -j${num_jobs}
 popd
