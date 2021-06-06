a941a9f8101ec356cace28efefa3fb281d566feb
Simon Julier <s.julier@ucl.ac.uk>
2021-06-06 17:14:53 +0100

    Separated path validation logic into separate script. Also create a blank tag to show the third party libraries are built.

--- Build.bat
+++ Build.bat
@@ -1,35 +1,9 @@
-@echo off
+@echo off 
 
-rem See if cmake can be found. If this fails, first try a fallback (default position)
-rem If the fallback fails, report an error and die.
+call Scripts\ValidateVCPKG.bat
 
-cmake --version >NUL 2>NUL
-
-if errorlevel 1 (
-    echo Cannot find cmake.exe on the command line; calling default vsvarsall.bat to install
-    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
-    cmake --version >NUL 2>NUL
-)
-
-if errorlevel 1 (
-    echo.
-    echo Cannot find cmake.exe; check that the cmake command line extensions are installed and
-    echo run vcvarsall.bat to register the build system
-    echo.
-    echo An example command line is:
-    echo.
-    echo "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
-    echo.
-    exit /b 1
-)
-
-rem Now check that VCPKG is installed
-
-if "%VCPKG_ROOT%"=="" (
-    echo.
-    echo Please set VCPKG_ROOT to the vcpkg installation directory.
-    echo.
-    exit /b 1
+if not "%errorlevel%"=="0" (
+  exit /b %errorlevel%
 )
 
 rem Set the build type
@@ -54,7 +28,7 @@ if not "%build_type%" == "Debug" (
 
 rem Create the sysexits.h file if it doesn't exist
 set sysexits_file=Develop\%build_type%\include\sysexits.h
-echo sysexits_file=%sysexits_file%
+echo Creating sysexits.h file %sysexits_file%
 if not exist "%sysexits_file%\include" (
 	mkdir Develop\%build_type%\include >NUL 2>NUL
 	echo #ifndef _SYSEXITS_H_ > %sysexits_file%
--- install_vckpg_dependencies.bat
+++ install_vckpg_dependencies.bat
@@ -1,8 +1,9 @@
-if "%VCKPG_ROOT%"=="" (
-    echo.
-    echo Please set VCKPG_ROOT to the vcpkg installation directory.
-    echo.
-    exit /b 1
+@echo off 
+
+call Scripts\ValidateVCPKG.bat
+
+if not "%errorlevel%"=="0" (
+  exit /b %errorlevel%
 )
 
-%VCKPG_ROOT%\vcpkg install boost:x64-windows eigen3:x64-windows suitesparse:x64-windows
+%VCPKG_ROOT%\vcpkg install boost eigen3 suitesparse opencv4
