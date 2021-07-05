007782389b77d68375eee3d789f41e9947318a2e
Simon Julier <s.julier@ucl.ac.uk>
2021-07-05 12:07:19 +0100

    Use the binary vocabulary in the TUM example.

--- Source/Examples/Monocular/mono_tum.cc
+++ Source/Examples/Monocular/mono_tum.cc
@@ -51,7 +51,7 @@ int main(int argc, char **argv)
 
     // Create SLAM system. It initializes all system threads and gets ready to process frames.
     string settingsFile = string(DEFAULT_SETTINGS_DIRECTORY) + string("/") + string(argv[1]);	
-    ORB_SLAM2::System SLAM(DEFAULT_ORB_VOCABULARY,settingsFile,ORB_SLAM2::System::MONOCULAR,true);
+    ORB_SLAM2::System SLAM(DEFAULT_BINARY_ORB_VOCABULARY,settingsFile,ORB_SLAM2::System::MONOCULAR,true);
 
     // Vector for tracking time statistics
     vector<float> vTimesTrack;
