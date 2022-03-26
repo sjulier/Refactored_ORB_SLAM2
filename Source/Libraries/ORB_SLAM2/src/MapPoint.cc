dd3311b1efa24affa5fb759d7f6892871436cfde
Simon Julier <s.julier@ucl.ac.uk>
2022-03-26 15:44:16 +0000

    Use Windows modification from https://github.com/Phylliida/orbslam-windows

--- Source/Libraries/ORB_SLAM2/src/MapPoint.cc
+++ Source/Libraries/ORB_SLAM2/src/MapPoint.cc
@@ -20,7 +20,6 @@
 
 #include "MapPoint.h"
 #include "ORBmatcher.h"
-
 #include <mutex>
 
 using namespace ::std;
@@ -262,30 +261,36 @@ void MapPoint::ComputeDistinctiveDescriptors() {
   // Compute distances between them
   const std::size_t N = vDescriptors.size();
 
-  float Distances[N][N];
-  for (std::size_t i = 0; i < N; i++) {
-    Distances[i][i] = 0;
-    for (std::size_t j = i + 1; j < N; j++) {
-      int distij =
-          ORBmatcher::DescriptorDistance(vDescriptors[i], vDescriptors[j]);
-      Distances[i][j] = distij;
-      Distances[j][i] = distij;
-    }
-  }
 
-  // Take the descriptor with least median distance to the rest
-  int BestMedian = INT_MAX;
-  int BestIdx = 0;
-  for (std::size_t i = 0; i < N; i++) {
-    std::vector<int> vDists(Distances[i], Distances[i] + N);
-    sort(vDists.begin(), vDists.end());
-    int median = vDists[0.5 * (N - 1)];
+	std::vector<std::vector<float> > Distances;
+	Distances.resize(N, vector<float>(N, 0));
+	for (size_t i = 0; i<N; i++)
+	{
+		Distances[i][i] = 0;
+		for (size_t j = i + 1;j<N;j++)
+		{
+			int distij = ORBmatcher::DescriptorDistance(vDescriptors[i], vDescriptors[j]);
+			Distances[i][j] = distij;
+			Distances[j][i] = distij;
+		}
+	}
 
-    if (median < BestMedian) {
-      BestMedian = median;
-      BestIdx = i;
+
+    // Take the descriptor with least median distance to the rest
+    int BestMedian = INT_MAX;
+    int BestIdx = 0;
+    for(size_t i=0;i<N;i++)
+    {
+        vector<int> vDists(Distances[i].begin(),Distances[i].end());
+        sort(vDists.begin(),vDists.end());
+        int median = vDists[0.5*(N-1)];
+
+        if(median<BestMedian)
+        {
+            BestMedian = median;
+            BestIdx = i;
+        }
     }
-  }
 
   {
     unique_lock<mutex> lock(mMutexFeatures);
