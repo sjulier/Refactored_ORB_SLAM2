79c047b8dc537cfd60498b784a105a7ed74091e2
Simon Julier <s.julier@ucl.ac.uk>
2022-03-26 16:35:38 +0000

    Change build directory to separate from thirdparty libraries.

--- Scripts/Build_Source.bat
+++ Scripts/Build_Source.bat
@@ -1,7 +1,7 @@
 echo Build type %build_type%
 set root_dir=%cd%
-mkdir "Build/%build_type%/Source"
-pushd "Build/%build_type%/Source"
-cmake.exe %root_dir%\Source -DCMAKE_BUILD_TYPE=%build_type% -G"NMake Makefiles" -DCMAKE_TOOLCHAIN_FILE=%toolchain_file%
+mkdir "Build/%build_type%/Source/ORBSLAM_2"
+pushd "Build/%build_type%/Source/ORBSLAM_2"
+cmake.exe %root_dir%\Source -DCMAKE_BUILD_TYPE=%build_type% -G"NMake Makefiles" -DCMAKE_TOOLCHAIN_FILE=%toolchain_file% -DCMAKE_C_FLAGS="-bigoj" -DCMAKE_CXX_FLAGS="-bigobj"
 nmake
 popd
