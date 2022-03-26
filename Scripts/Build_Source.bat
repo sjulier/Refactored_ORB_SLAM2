32b670f8c4a4c0506e6a0e085880dba152b5b8b9
Simon Julier <s.julier@ucl.ac.uk>
2022-03-26 17:00:18 +0000

    Use msbuild to support parallel builds. Unzip the compressed vocabulary file.

--- Scripts/Build_Source.bat
+++ Scripts/Build_Source.bat
@@ -1,7 +1,15 @@
+rem Check if the vocabulary file exists. If not, uncompress it
+
+if not exist %~dp0\..\Source\Vocabulary\ORBvoc.txt (
+
+   powershell -command "Expand-Archive -Force %~dp0..\Source\Vocabulary\ORBvoc.txt.zip %~dp0..\Source\Vocabulary"
+
+)
+
 echo Build type %build_type%
 set root_dir=%cd%
 mkdir "Build/%build_type%/Source/ORBSLAM_2"
 pushd "Build/%build_type%/Source/ORBSLAM_2"
-cmake.exe %root_dir%\Source -DCMAKE_BUILD_TYPE=%build_type% -G"NMake Makefiles" -DCMAKE_TOOLCHAIN_FILE=%toolchain_file% -DCMAKE_C_FLAGS="-bigoj" -DCMAKE_CXX_FLAGS="-bigobj"
-nmake
+cmake.exe %root_dir%\Source -DCMAKE_BUILD_TYPE=%build_type% -G"Visual Studio 16 2019" -DCMAKE_TOOLCHAIN_FILE=%toolchain_file% -DCMAKE_C_FLAGS="-bigoj" -DCMAKE_CXX_FLAGS="-bigobj -EHsc"
+msbuild INSTALL.vcxproj -maxcpucount:4
 popd
--- Scripts/Build_ThirdParty.bat
+++ Scripts/Build_ThirdParty.bat
@@ -13,8 +13,8 @@ echo Build type %build_type%
 set root_dir=%cd%
 mkdir "Build/%build_type%/Source/ThirdParty"
 pushd "Build/%build_type%/Source/ThirdParty"
-cmake.exe %root_dir%\Source\ThirdParty -DCMAKE_BUILD_TYPE=%build_type% -G"NMake Makefiles" -DCMAKE_TOOLCHAIN_FILE=%toolchain_file%
-nmake
+cmake.exe %root_dir%\Source\ThirdParty -DCMAKE_BUILD_TYPE=%build_type% -G"Visual Studio 16 2019" -DCMAKE_TOOLCHAIN_FILE=%toolchain_file%
+msbuild ALL_BUILD.vcxproj -maxcpucount:4
 popd
 
 rem Add the empty file which confirms the third party libraries have been built; only do this if nmake returned no error
